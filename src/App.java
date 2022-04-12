3020863331b73ad92205ce4329d1aca356e34d86
EarnestAndrei <100452486+EarnestAndrei@users.noreply.github.com>
2022-04-12 11:34:08 +0800

    code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,79 @@
+import java.util.Scanner;
+
 public class App {
+    /**
+     * This method takes in two int parameters to add.
+     * 
+     */
+    private static int computeSum(int num1, int num2){
+        return num1 + num2;
+    }
+
+    /**
+     * This method takes in 3 int parameters to add.
+     * 
+     */
+    private static int computeSum(int num1, int num2, int num3){
+        return num1 + num2 + num3;
+    }
+
+    /**
+     * Modify the methods in such a way that the Class App
+     * can now have 2 overloaded computeSum() methods.
+     * One computing the sum of two numbers, and one computing the sum
+     * of three numbers. 
+     */
+
+
+    /**
+     * 
+     * Create method that returns the greater integer value
+     */
+
+    private static int greaterValue(int num1, int num2){
+        if (num1>num2){
+            return num1;
+        }
+        else{
+            return num2;
+        }
+    }
+    /**
+     * 
+     * Create method that returns the greater double value
+     */
+
+    private static double greaterValue(double num1, double num2){
+        if (num1>num2){
+            return num1;
+        }
+        else{
+            return num2;
+        }
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        /**
+         * Create Method calls that takes in 2 parameters, and 3 parameters
+         * respectively and computes their sum.
+         */
+        Scanner scan = new Scanner(System.in);
+        int n1 = scan.nextInt();
+        int n2 = scan.nextInt();
+        int n3 = scan.nextInt();
+        int n4 = scan.nextInt();
+        int n5 = scan.nextInt();
+        System.out.println("The Sum of "+n1+" and "+n2+" is "+computeSum(n1, n2));
+        System.out.println("The Sum of "+n3+", "+n4+" and "+n5+" is "+computeSum(n3,n4,n5));
+        /**
+         * Call the greaterValue method with integer parameters
+         */
+        System.out.println("The greater value between 45 and 100 is " + greaterValue(45,100));
+
+        /**
+         * Call the greaterValue method with double parameters
+         */
+        System.out.println("The greater value between 3.14 and 9.8 is " + greaterValue(3.14,9.8));
+
     }
 }
