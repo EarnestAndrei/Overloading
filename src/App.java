71e5f84a6ffe95c01daf2b2113dafd4e5f377c73
EarnestAndrei <100452486+EarnestAndrei@users.noreply.github.com>
2022-04-12 11:42:54 +0800

    refinements and corrections and comments

--- src/App.java
+++ src/App.java
@@ -1,79 +1,35 @@
-import java.util.Scanner;
-
 public class App {
-    /**
-     * This method takes in two int parameters to add.
-     * 
-     */
-    private static int computeSum(int num1, int num2){
-        return num1 + num2;
+    private static int computeSum(int num1, int num2){//adds 2 integers
+        return num1 + num2;//returns sum
+    }
+    private static int computeSum(int num1, int num2, int num3){//adds 3 integers
+        return num1 + num2 + num3;//returns sum
     }
 
-    /**
-     * This method takes in 3 int parameters to add.
-     * 
-     */
-    private static int computeSum(int num1, int num2, int num3){
-        return num1 + num2 + num3;
-    }
-
-    /**
-     * Modify the methods in such a way that the Class App
-     * can now have 2 overloaded computeSum() methods.
-     * One computing the sum of two numbers, and one computing the sum
-     * of three numbers. 
-     */
-
-
-    /**
-     * 
-     * Create method that returns the greater integer value
-     */
-
-    private static int greaterValue(int num1, int num2){
-        if (num1>num2){
-            return num1;
+    private static int greaterValue(int num1, int num2){//compares 2 integers
+        if (num1>num2){//checks if the the first integer is greater
+            return num1;//returns the first integer
         }
         else{
-            return num2;
+            return num2;//returns the second integer
         }
     }
-    /**
-     * 
-     * Create method that returns the greater double value
-     */
 
-    private static double greaterValue(double num1, double num2){
-        if (num1>num2){
-            return num1;
+    private static double greaterValue(double num1, double num2){//compares 2 doubles
+        if (num1>num2){//checks if the first double is greater
+            return num1;//returns the first double
         }
         else{
-            return num2;
+            return num2;//returns the second double
         }
     }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        /**
-         * Create Method calls that takes in 2 parameters, and 3 parameters
-         * respectively and computes their sum.
-         */
-        Scanner scan = new Scanner(System.in);
-        int n1 = scan.nextInt();
-        int n2 = scan.nextInt();
-        int n3 = scan.nextInt();
-        int n4 = scan.nextInt();
-        int n5 = scan.nextInt();
-        System.out.println("The Sum of "+n1+" and "+n2+" is "+computeSum(n1, n2));
-        System.out.println("The Sum of "+n3+", "+n4+" and "+n5+" is "+computeSum(n3,n4,n5));
-        /**
-         * Call the greaterValue method with integer parameters
-         */
-        System.out.println("The greater value between 45 and 100 is " + greaterValue(45,100));
-
-        /**
-         * Call the greaterValue method with double parameters
-         */
-        System.out.println("The greater value between 3.14 and 9.8 is " + greaterValue(3.14,9.8));
+        
+        System.out.println("The Sum of 1 and 2 is "+computeSum(1, 2));//prints the sum of 1+2
+        System.out.println("The Sum of 3, 4 and 5 is "+computeSum(3,4,5));//prints the sum of 3+4+5
+        
+        System.out.println("The greater value between 45 and 100 is " + greaterValue(45,100));//finds the greater value between 45 and 100
+        System.out.println("The greater value between 3.14 and 9.8 is " + greaterValue(3.14,9.8));//finds the greater value between 3.14 and 9.8
 
     }
 }
