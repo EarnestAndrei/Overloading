2f98eb7496a12d3313a7fac30e281f8afc64920c
EarnestAndrei <100452486+EarnestAndrei@users.noreply.github.com>
2022-04-12 12:13:34 +0800

    capitalization errors

--- src/App.java
+++ src/App.java
@@ -25,8 +25,8 @@ public class App {
     }
     public static void main(String[] args) throws Exception {
         
-        System.out.println("The Sum of 1 and 2 is "+computeSum(1, 2));//prints the sum of 1+2
-        System.out.println("The Sum of 3, 4 and 5 is "+computeSum(3,4,5));//prints the sum of 3+4+5
+        System.out.println("The sum of 1 and 2 is "+computeSum(1, 2));//prints the sum of 1+2
+        System.out.println("The sum of 3, 4 and 5 is "+computeSum(3,4,5));//prints the sum of 3+4+5
         
         System.out.println("The greater value between 45 and 100 is " + greaterValue(45,100));//finds the greater value between 45 and 100
         System.out.println("The greater value between 3.14 and 9.8 is " + greaterValue(3.14,9.8));//finds the greater value between 3.14 and 9.8
